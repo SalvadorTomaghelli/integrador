176ec8ddd2dd1e491c496f9026e72a0966c46dc8
TobiasColer <tcoler@udesa.edu.ar>
2024-09-25 21:45:16 -0300

    Ver Todas Cartelera

--- src/components/VerTodasCartelera/VerTodasCartelera.js
+++ src/components/VerTodasCartelera/VerTodasCartelera.js
@@ -7,34 +7,71 @@ class VerTodasCartelera extends Component {
         super(props)
         this.state={
             Peliculas: [],
-
+            PeliculasFiltradas: [],
+            Filtrado: '',
+            PaginaActual: 2
         }
     }
     componentDidMount(){
         fetch(this.props.url)
         .then(response => response.json())
         .then(data => {
-            console.log(data.results)
             this.setState(
-            {Peliculas: data.results}
+            {Peliculas: data.results, PeliculasFiltradas: data.results}
             
         )})
         .catch(error => console.log(error))
     }
+
+    handleFilter(e){
+        const userValue = e.target.value
+        this.setState({
+            Filtrado: userValue,
+            PeliculasFiltradas: this.state.Peliculas.filter((peliculas) => peliculas.title.toLowerCase().includes(userValue.toLowerCase()))
+        })
+    }
+
+    handleResetFilter(){
+        this.setState({
+            Filtrado: '',
+            PeliculasFiltradas: this.state.Peliculas
+        })
+    }
+
+    handleLoadMore(){
+        fetch(`https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=${this.state.PaginaActual}&sort_by=popularity.desc&with_release_type=2|3&release_date.gte={min_date}&release_date.lte={max_date}&api_key=26cb00ba0e4d52cae073a420c45e2d99`)
+        .then(response => response.json())
+        .then(data => {
+            this.setState(
+                {Peliculas: this.state.Peliculas.concat(data.results),PeliculasFiltradas: this.state.PeliculasFiltradas.concat(data.results), PaginaActual:this.state.PaginaActual +1}
+            )
+        })
+        .catch(error => console.log(error))
+    }
+
     render(){
-        const {Peliculas} = this.state
+        const {PeliculasFiltradas} = this.state
         return(
+            <>
+
+    <input type='text' value={this.state.Filtrado} onChange={(e) => this.handleFilter(e)}/>
+
+    <button onClick={() => this.handleResetFilter()}>Reset Filter</button>
+    <button onClick={() => this.handleLoadMore()}>Cargar Mas</button>
+
             <section className="cartelera-card">
                 {
-                    Peliculas.length > 0 ? (
-                        Peliculas.map((peliculas, idx)=>(
+                    PeliculasFiltradas.length > 0 ? (
+                        PeliculasFiltradas.map((peliculas, idx)=>(
                             <PeliculasCard key={idx} peliculas={peliculas} />
                         ))
                     ) : (
                         <p>Cargando...</p>
                     )
                 }
+                
             </section>
+            </>
         )
     }
 }
